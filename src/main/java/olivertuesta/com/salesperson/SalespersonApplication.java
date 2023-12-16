c3380c2cee0745a40694657956599cb810e95a8a
Oliver Tuesta <oliver.jtuesta@gmail.com>
2023-12-16 18:24:38 -0500

    first commit

---
+++ src/main/java/olivertuesta/com/salesperson/SalespersonApplication.java
@@ -0,0 +1,13 @@
+package olivertuesta.com.salesperson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalespersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalespersonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/olivertuesta/com/salesperson/SalespersonApplicationTests.java
@@ -0,0 +1,13 @@
+package olivertuesta.com.salesperson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalespersonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
